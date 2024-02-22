d6c73d8a4044d250bce83dc0748600b0d2e6167d
Koschei <nitianzero@gmail.com>
2024-02-22 16:43:30 +0800

    feat: implement sleep util

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+//
+// Created by Koschei on 2024/2/17.
+//
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Usage: sleep seconds\n");
+        exit(1);
+    }
+    sleep(atoi(argv[1]));
+    exit(0);
+}
